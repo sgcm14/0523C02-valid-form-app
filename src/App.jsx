1392e18c5f35d95e817f9986c97afdb639ce5710
sgcm14 <sgcm14@gmail.com>
2024-08-14 20:19:33 -0500

    resolution exam

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
-// import './App.css';
+import { useState } from "react";
+import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const [nombre, setNombre] = useState("");
+  const [color, setColor] = useState("");
+  const [error, setError] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    console.log("event: ", event);
+    event.preventDefault();
+
+    // Validaciones
+    if (nombre.trim().length < 3 || nombre.trim().startsWith(" ")) {
+      setError("Por favor chequea que la información sea correcta.");
+      setSubmitted(false);
+      return;
+    }
+    const hexColorPattern = /^#[0-9A-F]{6}$/i;
+    if (!hexColorPattern.test(color)) {
+      setError("Por favor chequea que la información sea correcta.");
+      setSubmitted(false);
+      return;
+    }
+
+    setError("");
+    setSubmitted(true);
+  };
+
   return (
     <>
       <div className="App">
-        <h1>Carga de estudiantes</h1>
-        <form></form>
-        <Card />
+        <h1>Elige un color</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Ingresa tu nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+
+          <input
+            type="text"
+            placeholder="Ingresa tu color favorito (formato HEX)"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button type="submit">ENVIAR</button>
+        </form>
+
+        {error && <div className="error">{error}</div>}
+
+        {submitted && !error && <Card nombre={nombre} color={color} />}
       </div>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,27 @@
-// import React from "react";
-
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({ nombre, color }) => {
+  console.log(nombre, color)
+  return (
+    <div className="card">
+      <h2>Hola {nombre}!</h2>
+      <p>Sabemos que tu color favorito es:</p>
+      <div
+        // style={{
+        //   backgroundColor: color,
+        //   width: "100px",
+        //   height: "50px",
+        //   margin: "10px auto",
+        //   borderRadius: "5px",
+        //   border: "inherit",
+        // }}
+        className="color-box"
+        style={{
+          backgroundColor: color,
+        }}
+      >
+        <span>{color}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
